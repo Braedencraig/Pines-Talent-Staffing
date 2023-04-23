b014819a64475dfc632ee2d93cfb68ea77eded95
Braeden Craig <braeden.craig@gmail.com>
2023-04-23 14:41:30 -0400

    update add glitch

---
+++ components/contactform.js
@@ -0,0 +1,124 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+export default function ContactForm() {
+  const [formValues, setFormValues] = useState({});
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-xl mx-auto">
+      <div className="mb-6">
+        <label htmlFor="firstName" className="block font-bold mb-2 text-white">
+          First Name
+        </label>
+        <input
+          {...register("firstName", { required: true })}
+          type="text"
+          id="firstName"
+          value={formValues.firstName}
+          onChange={(e) =>
+            setFormValues({ ...formValues, firstName: e.target.value })
+          }
+          className="w-full px-3 py-2 border border-gray-400  focus:outline-none focus:border-white-500 text-white bg-black"
+        />
+        {errors.firstName && (
+          <span className="text-[#a1c4a3]">This field is required</span>
+        )}
+      </div>
+      <div className="mb-6">
+        <label htmlFor="lastName" className="block font-bold mb-2 text-white">
+          Last Name
+        </label>
+        <input
+          value={formValues.lastName}
+          onChange={(e) =>
+            setFormValues({ ...formValues, lastName: e.target.value })
+          }
+          {...register("lastName", { required: true })}
+          type="text"
+          id="lastName"
+          className="w-full px-3 py-2 border border-gray-400  focus:outline-none focus:border-white-500 text-white bg-black"
+        />
+        {errors.lastName && (
+          <span className="text-[#a1c4a3]">This field is required</span>
+        )}
+      </div>
+      <div className="mb-6 flex w-full">
+        <div className="flex flex-col w-[50%]">
+          <label htmlFor="email" className="block font-bold mb-2 text-white">
+            Email
+          </label>
+          <input
+            value={formValues.email}
+            onChange={(e) =>
+              setFormValues({ ...formValues, email: e.target.value })
+            }
+            {...register("email", { required: true })}
+            type="email"
+            id="email"
+            className="px-3 py-2 border border-gray-400  focus:outline-none focus:border-white-500 mr-2 text-white bg-black"
+          />
+          {errors.email && (
+            <span className="text-[#a1c4a3]">This field is required</span>
+          )}
+        </div>
+        <div className="flex flex-col w-[50%]">
+          <label htmlFor="phone" className="block font-bold mb-2 text-white">
+            Phone
+          </label>
+          <input
+            {...register("phone", { required: true })}
+            type="tel"
+            value={formValues.phone}
+            onChange={(e) =>
+              setFormValues({ ...formValues, phone: e.target.value })
+            }
+            id="phone"
+            className="px-3 py-2 border border-gray-400  focus:outline-none focus:border-white-500 ml-2 text-white bg-black"
+          />
+          {errors.phone && (
+            <span className="text-[#a1c4a3]">This field is required</span>
+          )}
+        </div>
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="jobDescription"
+          className="block font-bold mb-2 text-white"
+        >
+          Job Description
+        </label>
+        <textarea
+          rows="6"
+          value={formValues.jobDescription}
+          onChange={(e) =>
+            setFormValues({ ...formValues, jobDescription: e.target.value })
+          }
+          {...register("jobDescription", { required: true })}
+          id="jobDescription"
+          className="w-full px-3 py-2 border border-gray-400  focus:outline-none focus:border-white-500 text-white bg-black"
+        />
+        {errors.jobDescription && (
+          <span className="text-[#a1c4a3]">This field is required</span>
+        )}
+      </div>
+      <div className="w-full m-auto flex justify-center">
+        <button
+          type="submit"
+          className="bg-white text-black px-4 py-2 w-[200px] hover:bg-[#a1c4a3] focus:outline-none focus:bg-[#a1c4a3]"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import { getAllPostsForHome } from "../lib/api";
 import Head from "next/head";
 import data from "../data.json";
 import classNames from "classnames";
+import FadeInSection from "../components/fadein";
+import ContactForm from "../components/contactform";
 
 export default function Index({ preview, allPosts }) {
   const { hero } = data;
@@ -101,15 +103,131 @@ export default function Index({ preview, allPosts }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>{`Pines Talent Staffing & Consulting`}</title>
+          <title>{`Home | Pines Talent Staffing & Consulting`}</title>
         </Head>
         <div className="hero mt-[72px]">
           <div className="flex flex-col text-center text-white">
-            <h1 className="text-[100px]">{hero.title}</h1>
-            <h2 className="text-5xl">{hero.subtitle}</h2>
+            <FadeInSection key={1}>
+              <h1 className="text-[100px]">{hero.title}</h1>
+              <h2 className="text-5xl">{hero.subtitle}</h2>
+            </FadeInSection>
           </div>
         </div>
-        <Container></Container>
+        <Container>
+          <FadeInSection
+            classNames="flex flex-col justify-center items-center text-center mt-20"
+            key={2}
+          >
+            <h3 className="text-4xl mb-6">People First Approach to Staffing</h3>
+            <h4 className="text-2xl mb-12">
+              At Pines Talent, we believe that people are at the heart of every
+              successful business.
+            </h4>
+          </FadeInSection>
+          <FadeInSection
+            classNames="flex flex-col justify-center items-center text-center mb-20 max-w-[1000px] m-auto text-xl"
+            key={3}
+          >
+            <p className="mb-6">
+              We work closely with both clients and candidates to ensure that we
+              understand their unique needs, values, and goals. With our
+              extensive network and personalized approach, we match the right
+              people with the right opportunities to build the strongest teams.
+            </p>
+            <p>
+              Whether you're looking for a job or looking for new talent to join
+              your team, we are here to make the experience quick and simple.
+              Take a look at our features and let us take the pressure off your
+              search.
+            </p>
+          </FadeInSection>
+          <FadeInSection key={4}>
+            <ContactForm />
+          </FadeInSection>
+          <div class="content">
+            <FadeInSection key={5}>
+              <figure class="item">
+                <div
+                  class="item__img glitch"
+                  style={{ "--img": "url(/assets/img1.jpg)" }}
+                  data-tilt
+                  data-tilt-max="15"
+                  data-tilt-speed="3000"
+                  data-tilt-perspective="1500"
+                  data-tilt-scale="0.95"
+                  data-tilt-mouse-event-element=".content .item:nth-child(1)"
+                >
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                </div>
+                <div
+                  class="item__cover"
+                  style={{ "background-image": "url(/assets/img1.jpg)" }}
+                ></div>
+                <figcaption class="item__content">
+                  <h3 class="item__content-title">Marketing & Creative</h3>
+                </figcaption>
+              </figure>
+            </FadeInSection>
+            <FadeInSection key={6}>
+              <figure class="item">
+                <div
+                  class="item__img glitch"
+                  style={{ "--img": "url(/assets/img2.jpg)" }}
+                  data-tilt
+                  data-tilt-max="15"
+                  data-tilt-speed="3000"
+                  data-tilt-perspective="1500"
+                  data-tilt-scale="0.95"
+                  data-tilt-mouse-event-element=".content .item:nth-child(1)"
+                >
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                </div>
+                <div
+                  class="item__cover"
+                  style={{ "background-image": "url(/assets/img2.jpg)" }}
+                ></div>
+                <figcaption class="item__content">
+                  <h3 class="item__content-title">Food & Beverage</h3>
+                </figcaption>
+              </figure>
+            </FadeInSection>
+            <FadeInSection key={7}>
+              <figure class="item">
+                <div
+                  class="item__img glitch"
+                  style={{ "--img": "url(/assets/img3.jpg)" }}
+                  data-tilt
+                  data-tilt-max="15"
+                  data-tilt-speed="3000"
+                  data-tilt-perspective="1500"
+                  data-tilt-scale="0.95"
+                  data-tilt-mouse-event-element=".content .item:nth-child(1)"
+                >
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                  <div class="glitch__img"></div>
+                </div>
+                <div
+                  class="item__cover"
+                  style={{ "background-image": "url(/assets/img3.jpg)" }}
+                ></div>
+                <figcaption class="item__content">
+                  <h3 class="item__content-title">VFX & Post Production</h3>
+                </figcaption>
+              </figure>
+            </FadeInSection>
+          </div>
+        </Container>
       </Layout>
       <Cursor />
     </>
